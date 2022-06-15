b81a56f10e12406100f051f329b219dff47a998e
Elena <elenna@mail.ru>
2022-06-15 22:45:34 +0300

    Task 10 completed, task 13 failed

---
+++ Seminar2Homework/Program.cs
@@ -0,0 +1,49 @@
+﻿// Task 10
+/*
+int FindMiddle(int num);
+*/
+
+/*
+Random rnd = new Random();
+int number = rnd.Next(100,999);
+Console.WriteLine(number);
+
+int num1, num2;
+
+num1 = number / 10;
+
+num2 = num1  % 10;
+
+Console.WriteLine(num2);
+*/
+
+// Task 13
+
+/*
+Random rnd = new Random();
+int number = rnd.Next();
+Console.WriteLine(number);
+
+int num1, num2, result;
+
+if(number % 1000 == 0)
+{
+    Console.WriteLine("No third digit");
+}
+else
+{
+   num1 = number / 10;
+    while(num1 > 999);
+    {
+        num2 = num1 / 10;
+        num1  = num2; 
+    }
+   result = num1 % 10;
+   Console.WriteLine(result);
+}
+
+*/
+
+// Task 15
+
+
