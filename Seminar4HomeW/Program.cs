eb4c7df787211f2b9ee7f23bf7bbbc9db9fccee8
Elena <elenna@mail.ru>
2022-06-21 13:27:42 +0300

    Seminar 4 Homework

---
+++ Seminar4HomeW/Program.cs
@@ -0,0 +1,32 @@
+﻿//Task 25 Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*
+double APowerB(int num)
+{
+    double num1, num2, power;
+    Console.Write("Input first digit");
+    num1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input second digit");
+    num2 = Convert.ToInt32(Console.ReadLine());
+    power = Math.Pow(num1,num2);
+    return power;
+}
+int result = APowerB(num1, num2);
+Console.WriteLine(result);
+
+*/
+
+//Task 29 Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+int size = 8;
+int[] array = new int[size];
+
+for(int i = 0; i < size; i++)
+{
+    array[i] = new Random().Next();
+}
+
+for(int i = 0; i < size; i++)
+{
+    Console.WriteLine(array[i]);
+}
+
